b6e6547dab9424baa8dd2534d8644649e1a87698
Denzeldeveloper <denzeldeveloper@gmail.com>
2021-06-21 11:52:04 -0700

    A program that prints all possible combinations of single-digit numbers.
        Numbers must be separated by ,, followed by a space
        Numbers should be printed in ascending order
        You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
        All your code should be in the main function
        You can only use putchar four times maximum in your code
        You are not allowed to use any variable of type char

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * * main - Print all possible combinations of single digit numbers
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+int i;
+for (i = 0; i<= 9; i++)
+	if (i < 9)
+{	putchar(i + '0');
+	putchar(',');
+	putchar(' ');
+}																				}															}
+putchar('\n');
+
+return(0);
+
+}
