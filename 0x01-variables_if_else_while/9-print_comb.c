22c5ea4ce49b858ba5df934955974232307ede5f
Denzeldeveloper <denzeldeveloper@gmail.com>
2021-08-03 04:03:03 -0700

    Changes to 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,15 +5,20 @@
  * *
  * * Return: Always 0 (Success)
  * */
+
 int main(void)
+
 {
-int i;
+
+  int i;
 for (i = 0; i<= 9; i++)
-	if (i < 9)
-{	putchar(i + '0');
+	if (i <= 9)
+
+	  {	putchar(i + '0');
 	putchar(',');
 	putchar(' ');
-}																				}															}
+	
+}																																	
 putchar('\n');
 
 return(0);
