419ebe2a20084db78ed99a4c4728fe6b7b0940d7
Denzeldeveloper <denzeldeveloper@gmail.com>
2021-08-03 00:38:34 -0700

    Changes to 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,10 @@ int main(void)
 
 			for (i = 'a'; i <= 'z'; i++)
 						putchar(i);
-					putchar('\n');
 					
 					for (i='A'; i<='Z'; i++)
 						putchar(i);
 					putchar('\n');
 
 					return (0);
-}
+					}
